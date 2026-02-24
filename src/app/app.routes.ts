bb783424f3e503a1a3eb3b8365518ec727e1fd9f
Dimby-Andrianina <dimbinekena533@gmail.com>
2026-02-24 08:46:45 +0100

    update dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,11 @@ export const routes: Routes = [
 		// pathMatch: 'full',
 		loadChildren: () => import('./storefront/storefront.routes').then(m => m.STOREFRONT_ROUTES)
 	},
+	{
+		path: 'owner',
+		loadChildren: () =>
+		import('./back-office/back-office.routes').then(m => m.BACK_OFFICE_ROUTES),
+	},
 
 
 	{ path: '**', redirectTo: '' }
