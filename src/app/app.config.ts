fadad44cc80bad57f2e1124f704e19b31b00d53e
Raitra <nasforger42@gmail.com>
2026-03-01 19:52:49 +0300

    fix: enhance StorefrontStateService to support platform detection for improved data fetching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { ShoppingCartOutline, UserOutline } from '@ant-design/icons-angular/icons';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay, withNoHttpTransferCache } from '@angular/platform-browser';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(withEventReplay(), withNoHttpTransferCache()),
     provideHttpClient(withFetch()),
     provideNzIcons([
       ShoppingCartOutline,
